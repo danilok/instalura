d1c05b09a1f3256d737f4712649ae3450158ba1c
Danilo Yorinori <daniloksy@gmail.com>
2021-07-29 23:26:30 -0300

    Add Global Style and AppHead to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,54 +1,15 @@
-import Head from 'next/head';
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import AppHead from '../src/components/core/AppHead';
+import { ThemeProvider } from 'styled-components'
 import theme from '../src/theme'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+import { GlobalStyle } from '../src/theme/GlobalStyle';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Instalura</title>
-        <meta name="title" content="Instalura" />
-        <meta name="description" content="Instalura - Compartilhe momentos e conecte-se com amigos" />
-        <meta property="og:title" content="Instalura" key="title" />
-        <meta property="og:description" content="Instalura - Compartilhe momentos e conecte-se com amigos" key="description" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://instalura-danilok.vercel.app" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+      <AppHead />
 
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
