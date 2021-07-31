8a199d05a92a40f2a94eca477965df22d549efa7
Danilo Yorinori <daniloksy@gmail.com>
2021-07-31 13:13:02 -0300

    Set smallestException variant to Button component

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components'
 import get from 'lodash/get'
+import { TextStyleVariantsMap } from '../../foundation/Text';
 
 const ButtonGhost = css`
   // arrow function
@@ -22,6 +23,9 @@ export const Button = styled.button`
   padding: 12px 26px;
   font-weight: bold;
   opacity: 1;
+
+  ${TextStyleVariantsMap.smallestException}
+
   border-radius: ${({ theme }) => theme.borderRadius};
   transition: ${({ theme }) => theme.transition};
   ${(props) => {
