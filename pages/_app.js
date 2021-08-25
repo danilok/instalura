c4be4c9688b410011b61380d606728c776c02780
Danilo Yorinori <daniloksy@gmail.com>
2021-08-25 20:28:20 -0300

    feat(_app): add SEO and AppHead components

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,16 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme';
 import GlobalStyle from '../src/theme/GlobalStyle';
+import AppHead from '../src/components/core/AppHead';
+import SEO from '../src/components/commons/SEO';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <AppHead />
+      <SEO headTitle="Home" />
+
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
