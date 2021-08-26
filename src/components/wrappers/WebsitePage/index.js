d32fcb9c65ec6d2e64db705dbb3d1d6f90b909fd
Danilo Yorinori <daniloksy@gmail.com>
2021-08-26 00:30:20 -0300

    feat(website-page): add WebsitePage component
    provide WebsitePageWrapper and WebsitePageContext with toggleModalCadastro

---
+++ src/components/wrappers/WebsitePage/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '../../foundation/layout/Box';
+import Footer from '../../commons/Footer';
+import Menu from '../../commons/Menu';
+import Modal from '../../commons/Modal';
+import FormCadastro from '../../patterns/FormCadastro';
+
+export const WebsitePageContext = React.createContext({
+  toggleModalCadastro: () => {},
+});
+
+export default function WebsitePageWrapper({ children }) {
+  const [isModalOpen, setModalState] = React.useState(false);
+
+  return (
+    <WebsitePageContext.Provider
+      value={{
+        toggleModalCadastro: () => {
+          setModalState(!isModalOpen);
+        },
+      }}
+    >
+      <Box
+        display="flex"
+        flex="1"
+        flexDirection="column"
+      >
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => {
+            setModalState(false);
+          }}
+        >
+          {(propsDoModal) => (
+            <FormCadastro propsDoModal={propsDoModal} />
+          )}
+        </Modal>
+
+        <Menu
+          onCadastrarClick={() => setModalState(true)}
+        />
+
+        {children}
+
+        <Footer />
+      </Box>
+    </WebsitePageContext.Provider>
+  );
+}
+
+WebsitePageWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
