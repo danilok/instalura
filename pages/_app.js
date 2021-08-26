8e0bc587c53de31e8fbe5086c63999a01ba81219
Danilo Yorinori <daniloksy@gmail.com>
2021-08-26 01:05:47 -0300

    refactor(_app): remove SEO component from _app
    it will be moved to WebsitePageWrapper

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,12 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme';
 import GlobalStyle from '../src/theme/GlobalStyle';
 import AppHead from '../src/components/core/AppHead';
-import SEO from '../src/components/commons/SEO';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <AppHead />
-      <SEO headTitle="Home" />
 
       <ThemeProvider theme={theme}>
         <GlobalStyle />
