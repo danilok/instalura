e8b6fd745875c0eefecad13c5c061bdc13c00c14
Danilo Yorinori <daniloksy@gmail.com>
2021-08-26 23:14:02 -0300

    refactor(sobre): refactor about page to use websitePageHOC for render AboutPage

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import Box from '../src/components/foundation/layout/Box';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function SobrePage() {
-  return <div>Sobre</div>;
+const Title = styled.h1`
+  text-align: center;
+`;
+
+function AboutPage() {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      flex="1"
+    >
+      <Title>Sobre</Title>
+    </Box>
+  );
 }
+
+export default websitePageHOC(AboutPage, {
+  pageWrapperProps: {
+    seoProps: {
+      headTitle: 'Sobre',
+    },
+  },
+});
