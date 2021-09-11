a88c1755afd946fc8b5fec16031942d45297a212
Danilo Yorinori <daniloksy@gmail.com>
2021-09-11 14:11:46 -0300

    fix(login-service): use working api url to login when deployed at vercel

--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -23,7 +23,8 @@ const BASE_URL = isStagingEnv
   // Back End de DEV
   ? 'https://instalura-api-git-master-omariosouto.vercel.app'
   // Back End de PROD
-  : 'https://instalura-api.omariosouto.vercel.app';
+  : 'https://instalura-api-git-master-omariosouto.vercel.app';
+  // : 'https://instalura-api.omariosouto.vercel.app';
 
 const loginService = {
   async login({ username, password },
