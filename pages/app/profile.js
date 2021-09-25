383b6f789391f97b9e8a89f0a31d7f030394df2e
Danilo Yorinori <daniloksy@gmail.com>
2021-09-25 12:52:57 -0300

    feat(profile-page): handle profile page authentication with authService

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -1,11 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import authService from '../../src/services/auth/authService';
 
-export default function ProfilePage() {
+export default function ProfilePage(props) {
   return (
     <div>
       Página de Profile!
+      <pre>
+        {JSON.stringify(props, null, 4)}
+      </pre>
       <img src="https://media.giphy.com/media/bn0zlGb4LOyo8/giphy.gif" alt="Nicolas Cage" />
     </div>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const auth = authService(ctx);
+  const hasActiveSession = await auth.hasActiveSession();
+
+  if (hasActiveSession) {
+    const session = await auth.getSession();
+    return {
+      props: {
+        user: session,
+      },
+    };
+  }
+
+  ctx.res.writeHead(307, { location: '/login/' });
+  ctx.res.end();
+
+  return {
+    props: {},
+  };
 }
