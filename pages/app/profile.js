8c61a340aba57dbd0b61f99eb27f694df80836c8
Danilo Yorinori <daniloksy@gmail.com>
2021-11-09 21:44:13 -0300

    refactor(profile-page): fix screen path and generate random numbers for followers/following

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 import authService from '../../src/services/auth/authService';
 import userService from '../../src/services/user/userService';
 import LoggedPageHOC from '../../src/components/wrappers/LoggedPage/hoc';
-import ProfileScreen from '../../src/components/screens/ProfileScreen';
+import ProfileScreen from '../../src/components/screens/app/ProfileScreen';
 
 function ProfilePage(props) {
   return (
@@ -27,17 +27,28 @@ export async function getServerSideProps(ctx) {
   const user = userService(ctx);
   const hasActiveSession = await auth.hasActiveSession();
 
+  const getRandomIntInclusive = (begin, end) => {
+    const min = Math.ceil(begin);
+    const max = Math.floor(end);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const followers = getRandomIntInclusive(0, 50000);
+  const following = getRandomIntInclusive(0, 50000);
+
   if (hasActiveSession) {
     const session = await auth.getSession();
     const profilePage = await user.getProfilePage();
     const posts = profilePage.posts && profilePage.posts.length > 0
-      ? profilePage.posts.slice(0, 25)
+      ? profilePage.posts
       : [];
     return {
       props: {
         user: {
           ...session,
           ...profilePage.user,
+          followers,
+          following,
         },
         posts,
       },
